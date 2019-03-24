b9008a4dae195459a1fb025584e8829577f43328
inessa luerce <idluerce@thoughtworks.com>
2019-03-24 15:34:14 -0300

    Aplica layout inicial de tela de cadastro

--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -4,118 +4,211 @@ class Cadastro extends Component {
   render() {
     return (
       <div className='cadastro'>
-        <h2>Cadastro</h2>
-        <form>
-          <div className='nome'>
+        <h2 className=''>Cadastro</h2>
+        <form className='form-cadastro'>
+          <div className='nome form-group'>
             <label>Nome Completo: </label>
-            <input type="text" name="nome" id="nome"></input>
+            <input className='form-control' type='text' name='nome' id='nome' />
           </div>
 
-          <div className='escolaridade'>
-            <label>Escolaridade:  </label>
-            <select>
-              <option value="vazio"> </option>
-              <option value="sem"> Sem Escolaridade </option>
-              <option value="ensf"> Ensino Fundamental </option>
-              <option value="ensfi"> Ensino Fundamental Incompleto </option>
-              <option value="ensf"> Ensino Fundamental Completo </option>
-              <option value="ensmi"> Ensino Médio Incompleto </option>
-              <option value="ensm"> Ensino Médio Completo </option>
-              <option value="ensti"> Ensino Técnico Incompleto </option>
-              <option value="enst"> Ensino Técnico Completo </option>
-              <option value="enssupi"> Ensino Superior Incompleto </option>
-              <option value="enssup"> Ensino Superior Completo </option>
+          <div className='escolaridade form-group'>
+            <label>Escolaridade: </label>
+            <select className='form-control'>
+              <option value='vazio'> </option>
+              <option value='sem'> Sem Escolaridade </option>
+              <option value='ensf'> Ensino Fundamental </option>
+              <option value='ensfi'> Ensino Fundamental Incompleto </option>
+              <option value='ensf'> Ensino Fundamental Completo </option>
+              <option value='ensmi'> Ensino Médio Incompleto </option>
+              <option value='ensm'> Ensino Médio Completo </option>
+              <option value='ensti'> Ensino Técnico Incompleto </option>
+              <option value='enst'> Ensino Técnico Completo </option>
+              <option value='enssupi'> Ensino Superior Incompleto </option>
+              <option value='enssup'> Ensino Superior Completo </option>
             </select>
           </div>
 
-          <div className='etnia'>
-            <label>Etnia:  </label>
-            <select>
-              <option value="vazio"> </option>
-              <option value="branco"> Branco </option>
-              <option value="negro"> Negro </option>
-              <option value="indio"> Indígena </option>
-              <option value="amarelo"> Amarelo </option>
-              <option value="pardo"> Pardo </option>
-              <option value="sem"> Prefere Não Declarar </option>
+          <div className='etnia form-group'>
+            <label>Etnia: </label>
+            <select className='form-control'>
+              <option value='vazio'> </option>
+              <option value='branco'> Branco </option>
+              <option value='negro'> Negro </option>
+              <option value='indio'> Indígena </option>
+              <option value='amarelo'> Amarelo </option>
+              <option value='pardo'> Pardo </option>
+              <option value='sem'> Prefere Não Declarar </option>
             </select>
           </div>
-          <div className='idade'>
+          <div className='idade form-group'>
             <label>Idade: </label>
-            <input type="number" name="idade" id="idade"></input>
+            <input
+              className='form-control'
+              type='number'
+              name='idade'
+              id='idade'
+            />
           </div>
 
-
-          <div className='ocupacao'>
+          <div className='ocupacao form-group'>
             <label>Ocupação: </label>
-            <input type="text" name="ocupacacao" id="ocupacao"></input>
+            <input
+              className='form-control'
+              type='text'
+              name='ocupacacao'
+              id='ocupacao'
+            />
           </div>
 
-          <div className='pessoas'>
+          <div className='pessoas form-group'>
             <label>Quantas pessoas moram com você? </label>
-            <input type="number" name="pessoas" id="pessoas"></input>
+            <input
+              className='form-control'
+              type='number'
+              name='pessoas'
+              id='pessoas'
+            />
           </div>
 
-          <div className='renda'>
-            <label>Renda Familiar:  </label>
-            <input type="number" name="renda" id="renda"></input>
+          <div className='renda form-group'>
+            <label>Renda Familiar: </label>
+            <input
+              className='form-control'
+              type='number'
+              name='renda'
+              id='renda'
+            />
           </div>
 
-          <div className='filhos'>
+          <div className='filhos form-group'>
             <label>Número de filhos: </label>
-            <input type="number" name="pessoas" id="pessoas"></input>
+            <input
+              className='form-control'
+              type='number'
+              name='pessoas'
+              id='pessoas'
+            />
           </div>
 
-          <div className='moradia'>
-            <label>Moradia: </label>
-            <input name="moradia" id="propria" value="propria" type="radio"></input>
-            <label for="propria"> Própria </label>
-            <input name="moradia" id="alugada" value="alugada" type="radio"></input>
-            <label for="alugada"> Alugada </label>
-            <input name="moradia" id="cedida" value="cedida" type="radio"></input>
-            <label for="cedida"> Cedida </label>
-            <input name="moradia" id="moradia" value="outros" type="radio"></input>
-            <label for="outros"> Outros </label>
+          <div className='moradia form-group'>
+            <label>Moradia: </label> <br />
+            <div className='radio'>
+              <input name='moradia' id='propria' value='propria' type='radio' />
+              <label for='propria'> Própria </label>
+            </div>
+            <div className='radio'>
+              <input name='moradia' id='alugada' value='alugada' type='radio' />
+              <label for='alugada'> Alugada </label>
+            </div>
+            <div className='radio'>
+              <input name='moradia' id='cedida' value='cedida' type='radio' />
+              <label for='cedida'> Cedida </label>
+            </div>
+            <div className='radio'>
+              <input name='moradia' id='moradia' value='outros' type='radio' />
+              <label for='outros'> Outros </label>
+            </div>
           </div>
-          <div className='endereco'>
+
+          <div className='endereco form-group'>
             <label>Endereço: </label>
-            <input type="text" name="endereco" id="endereco"></input>
+            <input
+              className='form-control'
+              type='text'
+              name='endereco'
+              id='endereco'
+            />
             <label>Bairro: </label>
-            <input type="text" name="bairro" id="bairro"></input>
+            <input
+              className='form-control'
+              type='text'
+              name='bairro'
+              id='bairro'
+            />
             <label>Número: </label>
-            <input type="text" name="numero" id="numero"></input>
+            <input
+              className='form-control'
+              type='text'
+              name='numero'
+              id='numero'
+            />
             <label>CEP: </label>
-            <input type="text" name="cep" id="cep"></input>
+            <input className='form-control' type='text' name='cep' id='cep' />
           </div>
 
-          <div className='medidas'>
+          <div className='medidas form-group'>
             <label>Circuferência Total do Busto (cm): </label>
-            <input type="number" name="busto" id="busto"></input>
+            <input
+              className='form-control'
+              type='number'
+              name='busto'
+              id='busto'
+            />
             <label>Circuferência Total da Cintura Alta (cm): </label>
-            <input type="number" name="cintura" id="cintura"></input>
+            <input
+              className='form-control'
+              type='number'
+              name='cintura'
+              id='cintura'
+            />
             <label>Circuferência do Quadril (cm): </label>
-            <input type="number" name="quadril" id="quadril"></input>
+            <input
+              className='form-control'
+              type='number'
+              name='quadril'
+              id='quadril'
+            />
             <label>Altura (m): </label>
-            <input type="number" name="altura" id="altura"></input>
+            <input
+              className='form-control'
+              type='number'
+              name='altura'
+              id='altura'
+            />
           </div>
 
+          <div className='genero form-group'>
+            <div className='radio'>
+              <input
+                name='genero'
+                id='feminino'
+                value='feminino'
+                type='radio'
+              />
+              <label for='feminino'> Feminino </label>
+            </div>
 
+            <div className='radio'>
+              <input
+                name='genero'
+                id='masculino'
+                value='masculino'
+                type='radio'
+              />
+              <label for='masculino'> Masculino </label>
+            </div>
 
-          <div className='genero'>
-            <input name="genero" id="feminino" value="feminino" type="radio"></input>
-            <label for="feminino"> Feminino </label>
+            <div className='radio'>
+              <input name='genero' id='outros' type='radio' />
+              <label for='outros'> Outros </label>
+            </div>
 
-            <input name="genero" id="masculino" value="masculino" type="radio"></input>
-            <label for="masculino"> Masculino </label>
-
-            <input name="genero" id="outros" type="radio" ></input>
-            <label for="outros"> Outros </label>
-            <input type="text"></input>
+            <div className='form-group'>
+              <label for='obervacoes'>OBS:</label>
+              <input
+                id='observacoes'
+                className='form-control'
+                type='textarea'
+              />
+            </div>
           </div>
           <div className='salvar'>
-            <input type="submit" value="Submit"></input>
+            <input
+              className='btn btn-primary btn-raised'
+              type='submit'
+              value='Submit'
+            />
           </div>
-
         </form>
       </div>
     )
