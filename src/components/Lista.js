5803d44d999e949cea2b93b2552c0e164b747f6d
inessa luerce <idluerce@thoughtworks.com>
2019-03-24 12:11:58 -0300

    [Isabel/Lidi/Fer/Ine] Adiciona rotas para menu

---
+++ src/components/Lista.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Lista extends Component {
+  render() {
+    return (
+      <div className='lista'>
+        <h2>Lista</h2>
+      </div>
+    )
+  }
+}
+
+export default Lista
