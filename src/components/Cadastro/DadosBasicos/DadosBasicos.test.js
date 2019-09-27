2062c844c1625f40f2c7f71d6b7d9cf856aad164
Gustavo Rech <gustavo.rech@thoughtworks.com>
2019-09-27 11:34:54 -0300

    Make DadosBasicos.test pass

--- src/components/Cadastro/DadosBasicos/DadosBasicos.test.js
+++ src/components/Cadastro/DadosBasicos/DadosBasicos.test.js
@@ -8,50 +8,48 @@ import Input from './../../UI/Input/Input';
 
 configure({adapter: new Adapter()})
 
-
 describe('<DadosBasicos />', () => {
-    let wrapper;
-    beforeEach(() => {
-        //wrapper = shallow(<DadosBasicos />);
-    });
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<DadosBasicos />);
+  });
 
-    it('should not render <Input /> given empty form', () => {
-       // expect(wrapper.find(Input)).toHaveLength(0);
-    });
+  it('should not render <Input /> given empty form', () => {
+    expect(wrapper.find(Input)).toHaveLength(0);
+  });
 
-/*
-    it('should ', () => {
-        wrapper.setProps({
-            data: {
-                name: {
-                    elementType: 'input',
-                    elementConfig: {
-                      type: 'text',
-                      label: 'Nome'
-                    },
-                    value: '',
-                    validation: {
-                      required: true
-                    },
-                    valid: false,
-                    touched: false
-                  },
-                  birth: {
-                    elementType: 'input',
-                    elementConfig: {
-                      type: 'number',
-                      label: 'Idade'
-                    },
-                    value: '',
-                    validation: {
-                      required: true
-                    },
-                    valid: false,
-                    touched: false
-                  }
-            }
-        });
-        expect(wrapper.find(Input)).toHaveLength(2);
+  it('should ', () => {
+    wrapper.setProps({
+      data: {
+        name: {
+          elementType: 'input',
+          elementConfig: {
+            type: 'text',
+            label: 'Nome'
+          },
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false,
+          touched: false
+        },
+        birth: {
+          elementType: 'input',
+          elementConfig: {
+            type: 'number',
+            label: 'Idade'
+          },
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false,
+          touched: false
+        }
+      }
     });
-    */
+    
+    expect(wrapper.find(Input)).toHaveLength(2);
+  });
 });
