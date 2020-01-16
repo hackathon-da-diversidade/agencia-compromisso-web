7357276fb65b2040e49dbfe9c287496b52299949
lucassveloso <lucas.s.veloso@hotmail.com>
2020-01-16 17:35:13 -0300

    [Lucas] Adiciona lint na pipeline do CircleCI

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,14 @@ class Header extends Component {
   render() {
     return (
       <div className={classes.Header}>
-        <Link to="/" className={classes.HeaderBack}>←</Link>
-        <img className={classes.HeaderIcon} src={icon}
-          alt="ícone estilizado com o perfil de uma mulher com uma fita métrica no black power"></img>
+        <Link to="/" className={classes.HeaderBack}>
+          ←
+        </Link>
+        <img
+          className={classes.HeaderIcon}
+          src={icon}
+          alt="ícone estilizado com o perfil de uma mulher com uma fita métrica no black power"
+        ></img>
         <h2 className={classes.HeaderTitle}>{this.props.title}</h2>
       </div>
     );
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,33 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import icon from '../assets/icon.png';
 import banner from '../assets/banner.png';
 
 class Menu extends Component {
   render() {
     return (
-      <div className='menu'>
-        <img src={icon} alt="ícone estilizado com o perfil de uma mulher com uma fita métrica no black power"></img>
+      <div className="menu">
+        <img
+          src={icon}
+          alt="ícone estilizado com o perfil de uma mulher com uma fita métrica no black power"
+        ></img>
         <img src={banner} alt="banner da Agência Compromisso"></img>
 
-        <ul className='agencia-btn-group'>
+        <ul className="agencia-btn-group">
           <li>
-            <a href='/cadastro' className='btn-label text-white'>
-              <div className='btn btn-secondary btn-raised'>
+            <a href="/cadastro" className="btn-label text-white">
+              <div className="btn btn-secondary btn-raised">
                 <p>Cadastro</p>
               </div>
             </a>
           </li>
           <li>
-            <a href='/lista' className=''>
-              <div className='btn btn-secondary btn-raised'>
+            <a href="/lista" className="">
+              <div className="btn btn-secondary btn-raised">
                 <p>Lista</p>
               </div>
             </a>
           </li>
         </ul>
       </div>
-    )
+    );
   }
 }
 
-export default Menu
+export default Menu;
