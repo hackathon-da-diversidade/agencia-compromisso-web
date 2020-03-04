c0a9e76ca29e40087c27fa2bbea67842427ab26b
astenstrasser <astenstrasser@gmail.com>
2020-03-04 17:25:05 -0300

    [Bruno, Alexandra, Renan] Corrige testes

--- src/components/Detail/Detail.test.js
+++ src/components/Detail/Detail.test.js
@@ -5,12 +5,13 @@ import '@testing-library/jest-dom/extend-expect';
 
 import Detail from './Detail';
 
-const detailWithAPIMock = data => {
+const { data } = require('./fixtures/model.json');
+
+const detailWithAPIMock = _ => {
   jest.mock('../../api/fitModelAPI', () => ({
     get: () =>
       new Promise((resolve, reject) => {
-        const data = { data };
-        resolve(data);
+        resolve(require('./fixtures/model.json'));
       }),
   }));
 
@@ -26,59 +27,14 @@ const detailWithAPIMock = data => {
 };
 
 it('match snapshot', async () => {
-  const data = {
-    id: '5e3351db14c1174edbc11ac3',
-    name: 'Maria',
-    birthday: '21/10/2010',
-    availability: 'MORNING',
-    phoneNumber: '(41)123123333',
-    projects: 'Projeto 1',
-    address: null,
-    genderExpression: 'FEMALE',
-    education: null,
-    guardianName: 'Pai da Maria',
-    guardianPhoneNumber: '(41)231231231',
-    sizes: {
-      totalBustCircumference: 100.0,
-      totalWaistCircumference: 100.0,
-      totalHipCircumference: 100.0,
-      height: 100.0,
-    },
-    socialInformation: null,
-    identifyAsLGBTQIA: false,
-  };
-
-  const { asFragment, getByText } = render(detailWithAPIMock(data));
+  const { asFragment, getByText } = render(detailWithAPIMock());
   await wait(() => getByText('Maria'));
 
   expect(asFragment()).toMatchSnapshot();
 });
 
 it('renders all main information', async () => {
-  const data = {
-    id: '5e3351db14c1174edbc11ac3',
-    name: 'Maria',
-    birthday: '21/10/2010',
-    genderExpression: 'FEMALE',
-  };
-
-  jest.mock('../../api/fitModelAPI', () => ({
-    get: () =>
-      new Promise((resolve, reject) => {
-        const data = { data };
-        resolve(data);
-      }),
-  }));
-
-  const match = { params: { id: '5e3351db14c1174edbc11ac3' } };
-
-  const location = {};
-
-  const { asFragment, getByText } = render(
-    <BrowserRouter>
-      <Detail match={match} location={location} />
-    </BrowserRouter>
-  );
+  const { asFragment, getByText } = render(detailWithAPIMock());
   await wait(() => getByText('Maria'));
 
   expect(getByText(data.name)).toBeVisible();
@@ -87,57 +43,9 @@ it('renders all main information', async () => {
   expect(getByText('Feminina')).toBeVisible();
 });
 
-it('renders all main information', async () => {
-  const data = {
-    id: '5e3351db14c1174edbc11ac3',
-    name: 'Maria',
-    birthday: '21/10/2010',
-    genderExpression: 'FEMALE',
-  };
+//
 
-  jest.mock('../../api/fitModelAPI', () => ({
-    get: () =>
-      new Promise((resolve, reject) => {
-        const data = { data };
-        resolve(data);
-      }),
-  }));
-
-  const match = { params: { id: '5e3351db14c1174edbc11ac3' } };
-
-  const location = {};
-
-  const { asFragment, getByText } = render(
-    <BrowserRouter>
-      <Detail match={match} location={location} />
-    </BrowserRouter>
-  );
-  await wait(() => getByText('Maria'));
-
-  expect(getByText(data.name)).toBeVisible();
-  expect(getByText('Idade:')).toBeVisible();
-  expect(getByText('Expressão de gênero:')).toBeVisible();
-  expect(getByText('Feminina')).toBeVisible();
-});
-
-// it('não renderiza label se não tem informação', () => {
-//   fitModelAPI.get(id);
-//   // const { asFragment } = render(<Detail />);
-//   // expect(asFragment()).toMatchSnapshot();
-// });
-
-// it('não renderiza botão de contatar se não tem telefone', () => {
-//   fitModelAPI.get(id);
-//   // const { asFragment } = render(<Detail />);
-//   // expect(asFragment()).toMatchSnapshot();
-// });
-
-// it('renderiza botão de contatar se tem telefone', () => {
-//   fitModelAPI.get(id);
-//   // const { asFragment } = render(<Detail />);
-//   // expect(asFragment()).toMatchSnapshot();
-// });
-
-// it('mostra alerta de sucesso caso registro do modelo seja salvo (registrationSuccessful) ', () => {
-//   fitModelAPI.get(id);
-// });
+// todo: 'não renderiza label se não tem informação'
+// todo: ' não renderiza botão de contatar se não tem telefone'
+// todo : 'renderiza botão de contatar se tem telefone'
+// todo: 'mostra alerta de sucesso caso registro do modelo seja salvo (registrationSuccessful)
