66d19b8f25e985c19fd24a600cf9e88f1846a368
victoria <victoriaelizabethaalves@gmail.com>
2020-05-07 14:42:39 -0300

    Rollback https

--- src/server/server.js
+++ src/server/server.js
@@ -9,23 +9,15 @@ const routes = require('./routes');
 const server = express();
 const port = process.env.PORT || 5000;
 
+const sslRedirect = require('heroku-ssl-redirect');
+
+server.use(sslRedirect());
+
 server.use(morgan('combined'));
 server.use(cookieParser());
 server.use(express.static(path.join(__dirname, '../../build')));
 
 server.use('', routes);
-
-server.get('*', (request, res) => {
-  if (
-    !request.secure 
-    // &&
-    // process.env.NODE_ENV &&
-    // process.env.NODE_ENV === 'production'
-  ) {
-    return res.redirect('https://' + request.headers.host + request.url);
-  }
-  res.sendFile(path.join(__dirname, '../../build/index.html'));
-});
 
 server.get('*', (_, res) => {
   res.sendFile(path.join(__dirname, '../../build/index.html'));
