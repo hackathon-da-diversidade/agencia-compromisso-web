85c523d9d1a99f8594cd5c29735bf4203198a612
astenstrasser <astenstrasser@gmail.com>
2020-05-20 18:37:57 -0300

    [Alexandra, Lucas] Split details

---
+++ src/components/Detail/components/MainInformation/MainInformation.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Information from 'components/UI/Information/Information';
+import { calculateAge } from 'utils/dateUtils';
+import { GENDER_EXPRESSION } from 'utils/constants';
+import './MainInformation.css';
+
+const MainInformation = ({ model }) => {
+  const { birthday, genderExpression, name } = model;
+
+  return (
+    <>
+      <div className="MainInformation">
+        <Information testId="name" label="Nome:" strong>
+          {name}
+        </Information>
+        {birthday && (
+          <Information label="Idade:" strong>
+            {calculateAge(birthday).toString()}
+          </Information>
+        )}
+      </div>
+      <Information label="Expressão de gênero:" strong>
+        {GENDER_EXPRESSION[genderExpression]}
+      </Information>
+    </>
+  );
+};
+
+MainInformation.propTypes = {
+  model: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    birthday: PropTypes.string,
+    genderExpression: PropTypes.string,
+  }),
+};
+
+export default MainInformation;
