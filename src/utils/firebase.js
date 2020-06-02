8d3c19f03b1b59f8571717e9e29015e4aa8fcb38
Stephano Ramos <stephano.ramos@thoughtworks.com>
2020-06-02 13:09:54 -0300

    [Stephano][#69] Uses environment variables for Firebase config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyClxGyYmrV6FVgkcOzeUKWlJ1yeGRuq4o4',
-  authDomain: 'agencia-compromisso-8bafc.firebaseapp.com',
-  databaseURL: 'https://agencia-compromisso-8bafc.firebaseio.com',
-  projectId: 'agencia-compromisso-8bafc',
-  storageBucket: 'agencia-compromisso-8bafc.appspot.com',
-  messagingSenderId: '61471362023',
-  appId: '1:61471362023:web:4b2c1333a8590329d7cb42',
-  measurementId: 'G-7XYVFX0PW2',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
